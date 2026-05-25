7d4663672ee67d6ba5ec3c8ccaf6368202759812
Manas-2007 <patidarmanas01@gmail.com>
2026-05-25 23:23:02 +0530

    Merge sort

---
+++ 15_Merge_Sort.cpp
@@ -0,0 +1,149 @@
+#include<iostream>
+using namespace std;
+
+//Ascending Merge Sort
+void Ascend_mergeSort(int *array,int start,int end)
+{
+    if(start<end)
+    {
+        int mid=start+(end-start)/2;
+
+        //Left subarray
+        Ascend_mergeSort(array,start,mid);
+
+        //Right subarray
+        Ascend_mergeSort(array,mid+1,end);
+
+        //Sizes of both arrays
+        int n1=mid-start+1;
+        int n2=end-mid;
+
+        //Temporary Left & Right arrays
+        int L[n1],R[n2];
+
+        //Copy data into Left & Right array
+        for(int i=0;i<n1;i++)
+        {
+            L[i]=array[start+i];
+        }
+        
+        for(int j=0;j<n2;j++)
+        {
+            R[j]=array[mid+1+j];
+        }
+
+
+        int i=0,j=0,k=start;
+
+        //Ascending Condition
+        while(i<n1 && j<n2)
+        {
+            if(L[i]<=R[j])
+            {
+                array[k]=L[i];
+                i++;
+            }
+            else
+            {
+                array[k]=R[j];
+                j++;
+            }
+            k++;
+        }
+
+        //Inserting remaining elements into array
+        while(i<n1)
+        {
+            array[k]=L[i];
+            i++;
+            k++;
+        }
+        while(j<n2)
+        {
+            array[k]=R[j];
+            j++;
+            k++;
+        }
+    }
+}
+
+//Descending Merge Sort
+void Descend_mergeSort(int *array,int start,int end)
+{
+    if(start<end)
+    {
+        int mid=start+(end-start)/2;
+
+        //Left subarray
+        Descend_mergeSort(array,start,mid);
+
+        //Right subarray
+        Descend_mergeSort(array,mid+1,end);
+
+        //Sizes
+        int n1=mid-start+1;
+        int n2=end-mid;
+
+        //Temporary arrays
+        int L[n1],R[n2];
+
+        //Entering data in arrays
+        for(int i=0;i<n1;i++)
+        {
+            L[i]=array[start+i];
+        }
+        for(int j=0;j<n2;j++)
+        {
+            R[j]=array[mid+1+j];
+        }
+
+        int i=0,j=0,k=start;
+
+        //Descending Condition
+        while(i<n1 && j<n2)
+        {
+            if(L[i]>=R[j])
+            {
+                array[k]=L[i];
+                i++;
+            }
+            else{
+                array[k]=R[j];
+                j++;
+            }
+            k++;
+        }
+
+        //Remaining Elements insertion
+        while(i<n1)
+        {
+            array[k]=L[i];
+            i++;
+            k++;
+        }
+        while(j<n2)
+        {
+            array[k]=R[j];
+            j++;
+            k++;
+        }
+    }
+}
+
+int main()
+{
+    int nums[5]={67,45,36,99,3};
+    cout<<"Ascending Order : ";
+    Ascend_mergeSort(nums,0,4);
+    for(int i=0;i<5;i++)
+    {
+        cout<<nums[i]<<" ";
+    }
+    cout<<"\nDescending Order : ";
+    Descend_mergeSort(nums,0,4);
+    for(int i=0;i<5;i++)
+    {
+        cout<<nums[i]<<" ";
+    }
+    return 0;
+}
--- demo.cpp
+++ demo.cpp
@@ -1,49 +1,62 @@
 #include<iostream>
 using namespace std;
-
-//Bubble Sort Algorithm
-void BubbleSort(int *array,int size)
+void mergesort(int *array,int start,int end)
 {
-    int countswap=0;
-    for(int i=0;i<size-1;i++)
+    if(start<end)
     {
-        bool isSwap=false;
-        for(int j=0;j<size-i-1;j++)
-        {
-            if(array[j]>array[j+1])
-            {
-                countswap++;
-                isSwap=true;
-                swap(array[j],array[j+1]);
-            }
-        }
-        if(!isSwap)
-        {
-            break;
-        }
-    }
+        int mid=start+(end-start)/2;
+        mergesort(array,start,mid);
+        mergesort(array,mid+1,end);
 
-    cout<<"\nTOTAL SWAPS : "<<countswap<<"\n";
+        int n1=mid-start+1;
+        int n2=end-mid;
+
+        int L[n1],R[n2];
+        for(int i=0;i<n1;i++)
+        {
+            L[i]=array[start+i];
+        }
+        for(int j=0;j<n2;j++)
+        {
+            R[j]=array[mid+1+j];
+        }
+
+        int i=0,j=0,k=start;
+        while(i<n1 && j<n2)
+        {
+            if(L[i]<=R[j])
+            {
+                array[k]=L[i];
+                i++;
+            }
+            else
+            {
+                array[k]=R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i<n1)
+        {
+            array[k]=L[i];
+            i++;
+            k++;
+        }
+        while(j<n2)
+        {
+            array[k]=R[j];
+            j++;
+            k++;
+        }        
+    }
 }
 int main()
 {
-    int size;
-    cout<<"Enter the size of the array : ";
-    cin>>size;
-    int *array=new int[size];
-    cout<<"Start entry : ";
-    for(int i=0;i<size;i++)
+    int marks[5]={56,43,7,3,57};
+    mergesort(marks,0,4);
+    for(int i=0;i<5;i++)
     {
-        cin>>array[i];
+        cout<<marks[i]<<" ";
     }
-    
-    BubbleSort(array,size);
-
-    cout<<"Your Sorted Elements : ";
-    for(int i=0;i<size;i++)
-    {
-        cout<<array[i]<<" ";
-    }
-
-    return 0;
 }
